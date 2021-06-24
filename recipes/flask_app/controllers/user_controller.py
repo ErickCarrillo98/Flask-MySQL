afeb52561466590c2c57662407db820d336069a9
ErickCarrillo98 <82337484+ErickCarrillo98@users.noreply.github.com>
2021-06-24 14:31:27 -0700

    Add files via upload

---
+++ recipes/flask_app/controllers/user_controller.py
@@ -0,0 +1,56 @@
+from flask import render_template, request, session, redirect
+from flask_bcrypt import Bcrypt
+
+from flask_app import app
+
+from ..models.user import User
+
+bcrypt = Bcrypt(app)
+
+
+
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+
+@app.route("/register", methods = ["POST"])
+def register():
+    if not User.register_validator(request.form):
+        return redirect("/")
+
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": pw_hash
+    }
+
+    user_id = User.create(data)
+
+    session['uuid'] = user_id
+
+    return redirect("/dashboard")
+
+
+@app.route("/login", methods = ["POST"])
+def login():
+    if not User.login_validator(request.form):
+        return redirect("/")
+
+    user = User.get_by_email({"email": request.form['email']})
+
+    session['uuid'] = user.id
+
+    return redirect("/dashboard")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+
+    return redirect("/")
